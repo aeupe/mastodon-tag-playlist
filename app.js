8cde8c0a34a86606abdf5fc08b2726a5f48bf4b7
aeupe <aeupe@tuta.io>
2017-11-11 14:52:39 +0200

    return url instead of redirecting

--- app.js
+++ app.js
@@ -13,20 +13,14 @@ app.use(express.static('static'))
 app.get('/:instance/:tag/:pages(\\d\\d?)/:offset(\\d\\d?)', (req,res)=>{
 	req.params.offset = Math.min(req.params.offset, max_offset)
 	req.params.pages = Math.min(req.params.pages, max_pages)
-	let		opts = {
-				body: {limit: 40},
-				json: true,
-				timeout: 60 * 1000
-			}, 
-			i=0, ids=[],
+	let		i=0, max_id, ids=[],
 			url = 'https://' + req.params.instance + 
 				'/api/v1/timelines/tag/' + req.params.tag +
 				'?limit=40'
 	function get(){
-		request.get(url, (err,_res,body)=>{
-			if ( err || _res.statusCode != 200 ) {
-				res.sendStatus(500)
-			} else {
+		request.get(max_id ? url + '&' + max_id : url, (err,_res,body)=>{
+			if ( err || _res.statusCode != 200 ) res.sendStatus(500)
+			else {
 				let toots = JSON.parse(body)
 				toots.forEach(toot=>{
 					if ( ids.length >= max_ids ) return
@@ -41,13 +35,15 @@ app.get('/:instance/:tag/:pages(\\d\\d?)/:offset(\\d\\d?)', (req,res)=>{
 							}
 						})
 					}
-					opts.body.max_id = toot.id
+					max_id = toot.id
 				})
 				if ( ids.length >= max_ids || 
 						++i >= req.params.offset + req.params.pages || 
 						!toots.length ) {
-					res.redirect('http://www.youtube.com/watch_videos?video_ids=' + 
-						ids.join(','))
+					res.send(
+						'http://www.youtube.com/watch_videos?video_ids=' + 
+						ids.join(',')
+					)
 				} else get()
 			}
 		})
