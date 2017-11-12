92f7c550bde8786bbc1e58e40f46821cc7516bf3
aeupe <aeupe@tuta.io>
2017-11-12 15:01:43 +0200

    renaming

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const	express = require('express'),
 		app = express(),
 		port = process.argv[2] || 8080
 
-app.use(express.static('public'))
+app.use(express.static('docs'))
 
 app.listen(port, ()=>{
 	console.log('Listening on port ' + port)
