4ca9bb7e86ed002f8bfef9e18cf66067b5785a13
aeupe <aeupe@tuta.io>
2017-11-12 21:05:59 +0200

    set new_tab on submit

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 $(()=>{
 	$('form').submit(e=>{
+		let new_tab = $('#new_tab:checked').length
 		$('.progress').removeClass('hidden');
 		window.mastoSearch(
 			$('#instance').val(),
@@ -8,9 +9,8 @@ $(()=>{
 			$('#offset').val(),
 			url=>{
 				if ( url ) {
-					if ( $('#new_tab:checked').length ) {
-						window.open(url)
-					} else location.href = url
+					if ( new_tab ) window.open(url)
+					else location.href = url
 				} else Materialize.toast(`Couldn't find any content`, 1000)
 				$('.progress').addClass('hidden')
 			}, 
